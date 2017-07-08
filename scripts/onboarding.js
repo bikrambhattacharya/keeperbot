58f87e3e8b1b61d78fac7db271bc85dbadc3de1a
Seth Etter <sethetter@gmail.com>
2017-07-08 17:19:09 -0500

    Update intro messages

--- scripts/onboarding.js
+++ scripts/onboarding.js
@@ -7,7 +7,7 @@ const introMessages = [
   "Welcome to the Open Wichita Slack! I'm keeperbot, a chatbot you can interact with. My goal is to help you get involved with the group and answer whatever questions you may have.",
   "If you're 100% new to the concept of \"civic hacking\", you should read our Civic Hacking 101 guide -> https://github.com/openwichita/docs/blob/master/civic-hacking-101.md",
   "To learn about how we operate as a group and really get involved, read our onboarding guide, which can be found at https://github.com/openwichita/docs/blob/master/onboarding-guide.md",
-  "All the most important information is contained within our `docs` repo, at https://github.com/openwichita/docs."
+  "Make sure to introduce yourself to the group in the #general channel as well! Try starting with your name, what brought you here, and what your skillset is."
 ]
 
 module.exports = (robot) => {
