d82e9e6256eb02cddeb50d27a29c81cf3bae1c39
Seth Etter <sethetter@gmail.com>
2017-07-08 17:17:13 -0500

    Remove whitespace from intro messages

--- scripts/onboarding.js
+++ scripts/onboarding.js
@@ -4,23 +4,10 @@
  */
 
 const introMessages = [
-  `
-    Welcome to the Open Wichita Slack! I'm keeperbot, a chatbot you can interact
-    with. My goal is to help you get involved with the group and answer whatever
-    questions you may have.
-  `,
-  `
-    If you're 100% new to the concept of "civic hacking", you should read our
-    Civic Hacking 101 guide -> https://github.com/openwichita/docs/blob/master/civic-hacking-101.md
-  `,
-  `
-    To learn about how we operate as a group and really get involved, read our
-    onboarding guide, which can be found at https://github.com/openwichita/docs/blob/master/onboarding-guide.md.
-  `,
-  `
-    All the most important information is contained within our \`docs\` repo,
-    at https://github.com/openwichita/docs.
-  `,
+  "Welcome to the Open Wichita Slack! I'm keeperbot, a chatbot you can interact with. My goal is to help you get involved with the group and answer whatever questions you may have.",
+  "If you're 100% new to the concept of \"civic hacking\", you should read our Civic Hacking 101 guide -> https://github.com/openwichita/docs/blob/master/civic-hacking-101.md",
+  "To learn about how we operate as a group and really get involved, read our onboarding guide, which can be found at https://github.com/openwichita/docs/blob/master/onboarding-guide.md",
+  "All the most important information is contained within our `docs` repo, at https://github.com/openwichita/docs."
 ]
 
 module.exports = (robot) => {
